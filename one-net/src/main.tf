c7dabe1145529867faa6c7e7bde868ec71a98b2c
AnnaAndreenko <anya.andreenko@mail.ru>
2025-07-06 14:29:46 +0300

    f

--- one-net/src/main.tf
+++ one-net/src/main.tf
@@ -13,8 +13,7 @@ provider "yandex" {
   cloud_id  = var.cloud_id
   folder_id = var.folder_id
   zone      = "ru-central1-a"
-  token = var.token
-}
+  }
 
 resource "yandex_vpc_network" "default" {
   name = "net-devops"
