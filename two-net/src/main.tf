5ef5f12a7776deccbfc218c04be88e926f4d6f80
AnnaAndreenko <anya.andreenko@mail.ru>
2025-07-07 20:37:44 +0300

    sd

--- two-net/src/main.tf
+++ two-net/src/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     yandex = {
       source  = "yandex-cloud/yandex"
-      version = ">= 0.80"
+      version = ">= 0.80.0"
     }
   }
 }
@@ -11,113 +11,105 @@ provider "yandex" {
   cloud_id  = var.cloud_id
   folder_id = var.folder_id
   zone      = "ru-central1-a"
-  service_account_key_file = var.sa_key_path
 }
 
-# VPC
-resource "yandex_vpc_network" "default" {
+# Переменные вынесены в variables.tf
+
+resource "yandex_vpc_network" "network" {
   name = "lamp-network"
 }
 
 resource "yandex_vpc_subnet" "public" {
   name           = "public-subnet"
   zone           = "ru-central1-a"
-  network_id     = yandex_vpc_network.default.id
-  v4_cidr_blocks = ["192.168.10.0/24"]
+  network_id     = yandex_vpc_network.network.id
+  v4_cidr_blocks = ["192.168.100.0/24"]
 }
 
-# Bucket и картинка
+# Object Storage
 resource "yandex_storage_bucket" "image_bucket" {
-  bucket     = var.bucket_name
   access_key = var.access_key
   secret_key = var.secret_key
+  bucket     = var.bucket_name
+
+  acl = "public-read"
+
+  website {
+    index_document = "index.html"
+  }
+}
+
+resource "yandex_storage_object" "image" {
+  access_key = var.access_key
+  secret_key = var.secret_key
+  bucket     = yandex_storage_bucket.image_bucket.bucket
+  key        = "image.jpg"
+  source     = "image.jpg" # Предполагается, что файл лежит рядом
 
   acl = "public-read"
 }
 
-resource "yandex_storage_object" "lamp_image" {
-  bucket     = yandex_storage_bucket.image_bucket.bucket
-  key        = "lamp.jpg"
-  source     = "lamp.jpg"
-  acl        = "public-read"
-  access_key = var.access_key
-  secret_key = var.secret_key
-}
+# 3 LAMP-инстанса
+resource "yandex_compute_instance" "lamp" {
+  count       = 3
+  name        = "lamp-${count.index}"
+  platform_id = "standard-v1"
+  zone        = "ru-central1-a"
 
-# Instance template
-resource "yandex_compute_instance_group" "lamp_group" {
-  name                = "lamp-group"
-  service_account_id  = var.instance_sa_id
-  instance_template {
-    platform_id = "standard-v1"
-    resources {
-      cores  = 2
-      memory = 2
-    }
-    boot_disk {
-      initialize_params {
-        image_id = "fd827b91d99psvq5fjit"
-      }
-    }
-    network_interface {
-      subnet_ids = [yandex_vpc_subnet.public.id]
-      nat        = true
-    }
+  resources {
+    cores  = 2
+    memory = 2
+  }
 
-    metadata = {
-      user-data = <<EOF
-#cloud-config
-packages:
-  - apache2
-runcmd:
-  - systemctl enable apache2
-  - systemctl start apache2
-  - echo '<html><body><h1>Hello from LAMP VM!</h1><img src="https://${yandex_storage_bucket.image_bucket.bucket}.storage.yandexcloud.net/lamp.jpg"></body></html>' > /var/www/html/index.html
+  boot_disk {
+    initialize_params {
+      image_id = "fd827b91d99psvq5fjit" # LAMP
+    }
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.public.id
+    nat       = true
+  }
+
+  metadata = {
+    ssh-keys = "ubuntu:${file("~/.ssh/id_ed25519.pub")}"
+    user-data = <<EOF
+#!/bin/bash
+echo "<h1>LAMP VM ${count.index}</h1><img src='https://${yandex_storage_bucket.image_bucket.bucket}.storage.yandexcloud.net/image.jpg' width='400' />" > /var/www/html/index.html
 EOF
-    }
-  }
-
-  scale_policy {
-    fixed_scale {
-      size = 3
-    }
-  }
-
-  allocation_policy {
-    zones = ["ru-central1-a"]
-  }
-
-  deploy_policy {
-    max_unavailable = 1
-    max_creating    = 1
-    max_expansion   = 1
-    max_deleting    = 1
-  }
-
-  health_check {
-    http_options {
-      port = 80
-      path = "/"
-    }
   }
 }
 
 # Load Balancer
-resource "yandex_lb_network_load_balancer" "lamp_lb" {
-  name = "lamp-nlb"
+resource "yandex_lb_target_group" "tg" {
+  name = "lamp-tg"
 
+  target {
+    subnet_id = yandex_vpc_subnet.public.id
+    address   = yandex_compute_instance.lamp[0].network_interface[0].ip_address
+  }
+  target {
+    subnet_id = yandex_vpc_subnet.public.id
+    address   = yandex_compute_instance.lamp[1].network_interface[0].ip_address
+  }
+  target {
+    subnet_id = yandex_vpc_subnet.public.id
+    address   = yandex_compute_instance.lamp[2].network_interface[0].ip_address
+  }
+}
+
+resource "yandex_lb_network_load_balancer" "lamp_lb" {
+  name        = "lamp-lb"
   listener {
-    name        = "http-listener"
-    port        = 80
+    name = "http"
+    port = 80
     target_port = 80
-    external_address_spec {
-      ip_version = "ipv4"
-    }
+    protocol = "tcp"
   }
 
   attached_target_group {
-    target_group_id = yandex_compute_instance_group.lamp_group.load_balancer_target_group_id
-
+    target_group_id = yandex_lb_target_group.tg.id
     healthcheck {
       name = "http"
       http_options {
