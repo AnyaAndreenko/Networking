9033ac8faac42ce38666ae27d8129180fa620c75
AnnaAndreenko <anya.andreenko@mail.ru>
2025-07-07 20:42:42 +0300

    fix

--- two-net/src/main.tf
+++ two-net/src/main.tf
@@ -11,6 +11,7 @@ provider "yandex" {
   cloud_id  = var.cloud_id
   folder_id = var.folder_id
   zone      = "ru-central1-a"
+  token     = var.token
 }
 
 # Переменные вынесены в variables.tf
--- two-net/src/variable.tf
+++ two-net/src/variable.tf
@@ -3,6 +3,7 @@ variable "folder_id" {}
 variable "bucket_name" {}
 variable "access_key" {}
 variable "secret_key" {}
+variable "token" {}
 variable "sa_key_path" {
-  default = "key.json"
+  default = "~/key.json"
 }
