a450f1247d39668e29f46ff245367d5593c6f1ac
AnnaAndreenko <anya.andreenko@mail.ru>
2025-07-10 21:46:42 +0300

    s

--- two-net/src/variable.tf
+++ two-net/src/variable.tf
@@ -1,8 +1,8 @@
 variable "cloud_id" {}
 variable "folder_id" {}
 variable "bucket_name" {}
-variable "access_key" {}
-variable "secret_key" {}
+#variable "access_key" {}
+#variable "secret_key" {}
 variable "token" {}
 variable "sa_key_path" {
   default = "~/key.json"
