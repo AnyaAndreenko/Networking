6d243b2878a9deebfafc928af87472ee4ffb5b28
AnnaAndreenko <anya.andreenko@mail.ru>
2025-07-12 12:35:57 +0300

    g

--- two-net/src/main.tf
+++ two-net/src/main.tf
@@ -10,7 +10,7 @@ terraform {
 provider "yandex" {
   cloud_id  = var.cloud_id
   folder_id = var.folder_id
-  service_account_key_file = file("~/secrets.auto.tfvars")
+  token     = var.token
   zone      = "ru-central1-a"
 }
 
