d121bc4eb23688fdb21015a563d7ba97149f05bc
AnnaAndreenko <anya.andreenko@mail.ru>
2025-07-13 12:08:38 +0300

    fic

--- three-net/src/main.tf
+++ three-net/src/main.tf
@@ -8,40 +8,32 @@ terraform {
 }
 
 provider "yandex" {
-  cloud_id  = var.cloud_id
-  folder_id = var.folder_id
-  zone      = "ru-central1-a"
-  token     = var.token
+  zone = "ru-central1-a" # Используется профиль CLI, настроенный через yc init
 }
 
+variable "bucket_name" {
+  default = "devops-ann-bucket-secure" # Уникальное имя
+}
+
+# KMS ключ
 resource "yandex_kms_symmetric_key" "bucket_key" {
-  name              = "bucket-key"
+  name              = "bucket-encryption-key"
   default_algorithm = "AES_256"
   rotation_period   = "8760h" # 1 год
 }
 
-resource "yandex_vpc_network" "default" {
-  name = "default-network"
-}
-
-resource "yandex_vpc_subnet" "default" {
-  name           = "default-subnet"
-  zone           = "ru-central1-a"
-  network_id     = yandex_vpc_network.default.id
-  v4_cidr_blocks = ["10.1.0.0/24"]
-}
-
+# Бакет с шифрованием и доступом из интернета
 resource "yandex_storage_bucket" "secure_bucket" {
-  bucket     = var.bucket_name
-  access_key = var.access_key
-  secret_key = var.secret_key
-
-  default_storage_class = "STANDARD"
-  max_size              = 10
+  bucket   = var.bucket_name
+  max_size = 10
 
   anonymous_access_flags {
-    read  = true
-    list  = false
+    read = true
+    list = false
+  }
+
+  website {
+    index_document = "index.html"
   }
 
   server_side_encryption_configuration {
@@ -51,9 +43,5 @@ resource "yandex_storage_bucket" "secure_bucket" {
         sse_algorithm     = "aws:kms"
       }
     }
-  }
-
-  website {
-    index_document = "index.html"
   }
 }
