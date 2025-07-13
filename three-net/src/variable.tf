eae9d7f1ab0373beb368951066d5b59b1771933a
AnnaAndreenko <anya.andreenko@mail.ru>
2025-07-13 11:40:11 +0300

    fix

---
+++ three-net/src/variable.tf
@@ -0,0 +1,10 @@
+variable "cloud_id" {}
+variable "folder_id" {}
+variable "token" {}
+
+variable "bucket_name" {
+  default = "devops-ann-bucket-secure"
+}
+
+variable "access_key" {}
+variable "secret_key" {}
