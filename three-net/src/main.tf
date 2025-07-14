15c00dce8a101d9e1f1435487e066a05ce866031
AnnaAndreenko <anya.andreenko@mail.ru>
2025-07-14 21:51:46 +0300

    d

--- three-net/src/main.tf
+++ three-net/src/main.tf
@@ -8,43 +8,32 @@ terraform {
 }
 
 provider "yandex" {
-  service_account_key_file = file("key.json")
+  token     = var.token
   cloud_id  = var.cloud_id
   folder_id = var.folder_id
   zone      = "ru-central1-a"
 }
 
-variable "bucket_name" {
-  default = "ann-bucket-secure" # Уникальное имя
+resource "random_id" "bucket_suffix" {
+  byte_length = 4
 }
 
-# KMS ключ
-resource "yandex_kms_symmetric_key" "bucket_key" {
-  name              = "bucket-encryption-key"
-  default_algorithm = "AES_256"
-  rotation_period   = "8760h" # 1 год
-}
-
-# Бакет с шифрованием и доступом из интернета
 resource "yandex_storage_bucket" "secure_bucket" {
-  bucket   = var.bucket_name
-  max_size = 10
+  bucket     = "devops-ann-bucket-${random_id.bucket_suffix.hex}"
+  access_key = var.access_key
+  secret_key = var.secret_key
+  max_size   = 10
 
   anonymous_access_flags {
-    read = true
-    list = false
+    read  = true
+    list  = false
   }
 
   website {
     index_document = "index.html"
   }
 
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        kms_master_key_id = yandex_kms_symmetric_key.bucket_key.id
-        sse_algorithm     = "aws:kms"
-      }
-    }
+  encryption {
+    kms_key_id = var.kms_key_id
   }
 }
