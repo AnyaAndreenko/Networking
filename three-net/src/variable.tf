57935078a8bfb42e3956fcd850d41109869d0093
AnnaAndreenko <anya.andreenko@mail.ru>
2025-07-14 22:00:59 +0300

    f

--- three-net/src/variable.tf
+++ three-net/src/variable.tf
@@ -5,6 +5,8 @@ variable "token" {}
 #variable "bucket_name" {
  # default = "devops-ann-bucket-secure"
 #}
-
+variable "kms_key_name" {
+  default = "secure-bucket-key"
+}
 variable "access_key" {}
 variable "secret_key" {}
